89db6ad13c1624f41f11135a1265f03d259f7998
Théo HUGUET <theo@huguet-groupe.com>
2026-03-11 10:56:08 +0100

    Ajout de commentaire

--- s_libc_project_1/src/s_stdio.s
+++ s_libc_project_1/src/s_stdio.s
@@ -2,20 +2,24 @@
 
 .global s_fread
 s_fread:
+    # Prologue de la fonction
     pushl   %ebp
     movl    %esp, %ebp
     pushl   %ebx
 
-    movl    8(%ebp),  %ecx
-    movl    12(%ebp), %eax
-    movl    16(%ebp), %edx
-    mull    %edx
-    movl    %eax, %edx
+    # Charger les paramètres depuis la pile
+    movl    8(%ebp),  %ecx    # buffer (adresse mémoire)
+    movl    12(%ebp), %eax    # taille à lire
+    movl    16(%ebp), %edx    # nombre d'éléments
+    mull    %edx              # calculer taille_totale = taille * nombre
+    movl    %eax, %edx        # mettre le résultat dans %edx
 
-    movl    20(%ebp), %ebx
-    movl    $3, %eax
-    int     $0x80
+    # Appel système read
+    movl    20(%ebp), %ebx    # file descriptor
+    movl    $3, %eax          # syscall read (3)
+    int     $0x80             # interruption système
 
+    # Épilogue
     popl    %ebx
     popl    %ebp
     ret
@@ -23,20 +27,24 @@ s_fread:
 
 .global s_fwrite
 s_fwrite:
+    # Prologue de la fonction
     pushl   %ebp
     movl    %esp, %ebp
     pushl   %ebx
 
-    movl    8(%ebp),  %ecx
-    movl    12(%ebp), %eax
-    movl    16(%ebp), %edx
-    mull    %edx
-    movl    %eax, %edx
+    # Charger les paramètres depuis la pile
+    movl    8(%ebp),  %ecx    # buffer (adresse mémoire)
+    movl    12(%ebp), %eax    # taille à écrire
+    movl    16(%ebp), %edx    # nombre d'éléments
+    mull    %edx              # calculer taille_totale = taille * nombre
+    movl    %eax, %edx        # mettre le résultat dans %edx
 
-    movl    20(%ebp), %ebx
-    movl    $4, %eax
-    int     $0x80
+    # Appel système write
+    movl    20(%ebp), %ebx    # file descriptor
+    movl    $4, %eax          # syscall write (4)
+    int     $0x80             # interruption système
 
+    # Épilogue
     popl    %ebx
     popl    %ebp
     ret
--- s_libc_project_1/src/s_string.s
+++ s_libc_project_1/src/s_string.s
@@ -6,15 +6,15 @@ s_strlen:
     movl    %esp, %ebp
     pushl   %esi
 
-    movl    8(%ebp), %esi
-    xorl    %eax, %eax
+    movl    8(%ebp), %esi          # Charger l'adresse de la string
+    xorl    %eax, %eax             # Initialiser le compteur à 0
 
 .s_strlen_loop:
-    movb    (%esi, %eax), %dl
-    cmpb    $0, %dl
-    je      .s_strlen_end
-    incl    %eax
-    jmp     .s_strlen_loop
+    movb    (%esi, %eax), %dl      # Charger le caractère courant
+    cmpb    $0, %dl                # Vérifier si c'est le caractère nul
+    je      .s_strlen_end          # Si oui, terminer
+    incl    %eax                   # Incrémenter le compteur
+    jmp     .s_strlen_loop         # Continuer la boucle
 
 .s_strlen_end:
     popl    %esi
@@ -28,24 +28,24 @@ s_strchr:
     movl    %esp, %ebp
     pushl   %esi
 
-    movl    8(%ebp), %esi
-    movb    12(%ebp), %cl
+    movl    8(%ebp), %esi          # Charger l'adresse de la string
+    movb    12(%ebp), %cl          # Charger le caractère à chercher
 
 .s_strchr_loop:
-    movb    (%esi), %dl
-    cmpb    %cl, %dl
-    je      .s_strchr_found
-    cmpb    $0, %dl
-    je      .s_strchr_notfound
-    incl    %esi
-    jmp     .s_strchr_loop
+    movb    (%esi), %dl            # Charger le caractère courant
+    cmpb    %cl, %dl               # Comparer avec le caractère cherché
+    je      .s_strchr_found        # Si égal, on l'a trouvé
+    cmpb    $0, %dl                # Vérifier si fin de string
+    je      .s_strchr_notfound     # Si oui, non trouvé
+    incl    %esi                   # Passer au caractère suivant
+    jmp     .s_strchr_loop         # Continuer la boucle
 
 .s_strchr_found:
-    movl    %esi, %eax
+    movl    %esi, %eax             # Retourner le pointeur
     jmp     .s_strchr_end
 
 .s_strchr_notfound:
-    xorl    %eax, %eax
+    xorl    %eax, %eax             # Retourner NULL
 
 .s_strchr_end:
     popl    %esi
